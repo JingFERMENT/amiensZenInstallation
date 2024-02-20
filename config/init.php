9a31d1ed8823429c97403f47b8ded3cec003b4e8
Jing ZHANG <jing.zhang.ferment@gmail.com>
2024-02-20 22:36:23 +0100

    add gitignore file

--- config/init.php
+++ config/init.php
@@ -12,10 +12,8 @@ define('REGEX_PASSWORD', '^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9]).{
 define ('REGEX_TELEPHONE','^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$');
 define('REGEX_BIRTHDATE', '^(19|20)\d\d[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])$');
 
-// key
-define ('SECRET_KEY', 's$hhshshshshsh34');
-
-// format image 
+// constants
+// image
 define('ARRAY_TYPES_MIMES', ['image/jpeg', 'image/png']);
 define('UPLOAD_MAX_SIZE', 8 * 1024 * 1024);
 
